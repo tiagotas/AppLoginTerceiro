e9843f156df8c5dff33111458cc43b6eb0c80901
TIAGO SILVA <frost.tiago@gmail.com>
2021-05-21 12:29:48 -0300

    Adicionar arquivos de projeto.

---
+++ AppLoginTerceiro/AppLoginTerceiro/View/Login.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+using AppLoginTerceiro.Model;
+
+namespace AppLoginTerceiro.View
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Login : ContentPage
+    {
+        App PropriedadesApp;
+
+        //List<DadosUsuario> list_usuario;
+
+        public Login()
+        {
+            InitializeComponent();
+
+            PropriedadesApp = (App)Application.Current;
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                string email = txt_email.Text;
+                string senha = txt_senha.Text;
+
+                if (PropriedadesApp.list_usuarios.Any(i => (i.Email == email && i.Senha == senha)))
+                {
+                    App.Current.Properties.Add("usuario_logado", email);
+                    App.Current.MainPage = new Protegida();
+                }
+                else
+                    throw new Exception("Dados incorretos, tente novamente.");                
+
+            } catch(Exception ex)
+            {
+                DisplayAlert("Ops!", ex.Message, "OK");
+            }
+        }
+    }
+}
---
+++ AppLoginTerceiro/AppLoginTerceiro/View/Protegida.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AppLoginTerceiro.View
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Protegida : ContentPage
+    {
+        public Protegida()
+        {
+            InitializeComponent();
+
+            lbl_boasvindas.Text = App.Current.Properties["usuario_logado"].ToString();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                App.Current.Properties.Remove("usuario_logado");
+                App.Current.MainPage = new Login();
+
+            } catch(Exception ex)
+            {
+                DisplayAlert("Ops", ex.Message, "OK");
+            }
+        }
+    }
+}
